0f23c62e9e682ea300ae49b15ae0248f962883b9
lzz <447089649@qq.com>
2018-06-25 16:55:11 +0800

    统一静态资源处理

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 // 程序入口
 
 var express = require('express')
+var bodyParser = require('body-parser')
+var art = require('express-art-template')
 var router = require('./router')
 
 
 var app = express()
+
+// 统一静态资源管理(开放权限)
+app.use('/public', express.static('./public'))
+app.use('/node_modules', express.static('./node_modules'))
+app.engine('html', require('express-art-template'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 
 app.use(router)
 app.listen(3000, () => {
